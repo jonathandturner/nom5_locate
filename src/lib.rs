867971f8f81606314c5d28e7b83174dd251f6ebf
Jonathan Turner <jonathan.d.turner@gmail.com>
2019-07-17 07:34:04 +1200

    Remove stray println

--- src/lib.rs
+++ src/lib.rs
@@ -321,7 +321,7 @@ where
         };
 
         match &result {
-            Ok(v) => {}
+            Ok(_) => {}
             Err(Err::Incomplete(_)) => println!("incomplete"),
             Err(Err::Error(_)) => println!("error"),
             Err(Err::Failure(_)) => println!("failure"),
@@ -354,7 +354,6 @@ where
             Some(0) => Err(Err::Error(E::from_error_kind(self.clone(), e))),
             Some(n) => Ok(self.take_split(n)),
             None => {
-                println!("{:?}", self);
                 if self.fragment.input_len() == 0 {
                     Err(Err::Error(E::from_error_kind(self.clone(), e)))
                 } else {
